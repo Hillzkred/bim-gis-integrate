a64e1c947ed34dd5141f9372b33c040ef37f6c83
Alexis Almacin <AAlmacin@cimslab.ca>
2023-02-14 11:55:44 -0500

    Moved .wasm file to root directory

--- src/App.tsx
+++ src/App.tsx
@@ -7,8 +7,8 @@ import { IFCLoader } from "web-ifc-three";
 import { IFCModel } from "web-ifc-three/IFC/components/IFCModel";
 
 function App() {
-  const [ifcModel, setIfcModel] = useState({} as IFCModel);
   const [id, setId] = useState(Number);
+  const [ifcModel, setIfcModel] = useState({} as IFCModel);
   const [ifcModelsArray, setIfcModelsArray] = useState(Array<Object3D>);
 
   const ifcLoader = new IFCLoader();
@@ -18,39 +18,35 @@ function App() {
     const IfcFileFromEvent = event.target.files as FileList;
     const url = URL.createObjectURL(IfcFileFromEvent[0]);
     ifcLoader.load(url, (ifcModel) => {
+      console.log(ifcModel);
       setIfcModel(ifcModel);
       setIfcModelsArray([...ifcModelsArray, ifcModel]);
     });
   };
 
-  const handleIfcClick = (event: ThreeEvent<MouseEvent>) => {
-    const index = event.faceIndex as number;
-    const ifcObject = event.object as IFCModel;
-    const geometry = ifcObject.geometry as BufferGeometry;
-    const id = ifc.getExpressId(geometry, index);
-    setId(id);
-    console.log(id);
-  };
+  // const handleIfcClick = (event: ThreeEvent<MouseEvent>) => {
+  //   const index = event.faceIndex as number;
+  //   const ifcObject = event.object as IFCModel;
+  //   const geometry = ifcObject.geometry as BufferGeometry;
+  //   const id = ifc.getExpressId(geometry, index);
+  //   setId(id);
+  //   console.log(id);
+  // };
 
-  const handlePointerEnter = (event: ThreeEvent<MouseEvent>) => {
-    const ifcObject = event.object as IFCModel;
-    const modelId = ifcObject.modelID;
+  // const handlePointerEnter = (event: ThreeEvent<MouseEvent>) => {
+  //   const ifcObject = event.object as IFCModel;
+  //   const modelId = ifcObject.modelID;
 
-    const index = event.faceIndex as number;
-    const geometry = ifcObject.geometry as BufferGeometry;
-    const id = ifc.getExpressId(geometry, index);
-  };
+  //   const index = event.faceIndex as number;
+  //   const geometry = ifcObject.geometry as BufferGeometry;
+  //   const id = ifc.getExpressId(geometry, index);
+  // };
 
   return (
     <div className="h-screen w-screen">
       <ul className="flex bg-slate-600 text-white p-2 ">
         <li>
-          <input
-            type="file"
-            name="load"
-            className=""
-            onChange={handleIfcUpload}
-          />
+          <input type="file" name="load" onChange={handleIfcUpload} />
         </li>
         <li>
           <p>ID: {id}</p>
@@ -61,12 +57,7 @@ function App() {
           camera={{ fov: 75, near: 0.1, far: 1000, position: [8, 13, 15] }}
           raycaster={{ firstHitOnly: true }}
         >
-          <Select
-            box
-            multiple
-            onClick={handleIfcClick}
-            onPointerEnter={handlePointerEnter}
-          >
+          <Select box multiple>
             <mesh>
               <primitive object={ifcModel} />
             </mesh>
