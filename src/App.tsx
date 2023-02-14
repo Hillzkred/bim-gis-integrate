2d5a9b69a614d434966de916f6688581d9b54a35
Alexis Almacin <AAlmacin@cimslab.ca>
2023-02-14 10:15:34 -0500

    commit

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import "./App.css";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <div className="text-6xl text-cyan-500">
+      <h1>BIM+GIS Integration</h1>
+    </div>
+  );
+}
+
+export default App;
