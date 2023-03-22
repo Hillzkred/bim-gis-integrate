1e4aa30bf834cf6265b613f9f721122e09c9ad63
Alexis Almacin <AAlmacin@cimslab.ca>
2023-03-22 14:26:50 -0400

    experimenting with react-map-gl's Layer component's props

--- src/components/MapEnvironment.tsx
+++ src/components/MapEnvironment.tsx
@@ -1,5 +1,5 @@
-import Map, { Layer, LngLatLike } from "react-map-gl";
-import maplibregl from "maplibre-gl";
+import Map, { Layer, LngLatLike, Source } from "react-map-gl";
+import maplibregl, { MercatorCoordinate } from "maplibre-gl";
 import { Three } from "./ThreeEnv";
 import { ChangeEvent, useEffect, useState } from "react";
 import { Event } from "three";
@@ -7,6 +7,18 @@ import { Canvas } from "@react-three/fiber";
 import { IFCLoader } from "web-ifc-three";
 import { IfcModel } from "web-ifc-three/IFC/BaseDefinitions";
 import { IFCModel } from "web-ifc-three/IFC/components/IFCModel";
+import ThreeFiber from "./ThreeFiber";
+import {
+  PerspectiveCamera,
+  Scene,
+  DirectionalLight,
+  AmbientLight,
+  Vector3,
+  Matrix4,
+  WebGLRenderer,
+  // Box3,
+  AxesHelper,
+} from "three";
 
 function MapEnvironment() {
   const [ifcUrl, setIfcUrl] = useState("");
@@ -27,6 +39,61 @@ function MapEnvironment() {
   //   });
   // }, []);
 
+  const modelOrigin: LngLatLike = [2.0283, 48.9244];
+  const modelAltitude = 0;
+  const modelRotate = [Math.PI / 2, 0, 0];
+
+  const modelAsMercatorCoordinate: MercatorCoordinate =
+    maplibregl.MercatorCoordinate.fromLngLat(modelOrigin, modelAltitude);
+  const modelTransform: any = {
+    translateX: modelAsMercatorCoordinate.x,
+    translateY: modelAsMercatorCoordinate.y,
+    translateZ: modelAsMercatorCoordinate.z,
+    rotateX: modelRotate[0],
+    rotateY: modelRotate[1],
+    rotateZ: modelRotate[2],
+    scale: modelAsMercatorCoordinate.meterInMercatorCoordinateUnits(),
+  };
+
+  const handleLoad = (map: mapboxgl.Map, gl: WebGLRenderingContext) => {
+    this.camera = new PerspectiveCamera();
+    this.scene = new Scene();
+
+    const axes = new AxesHelper(10);
+    axes.renderOrder = 3;
+    this.scene.add(axes);
+
+    // create three.js lights to illuminate the model
+    const lightColor = 0xffffff;
+    const ambientLight = new AmbientLight(lightColor, 0.2);
+    this.scene.add(ambientLight);
+
+    const directionalLight = new DirectionalLight(lightColor, 0.9);
+    directionalLight.position.set(0, -700, 600).normalize();
+    this.scene.add(directionalLight);
+
+    const directionalLight2 = new DirectionalLight(lightColor, 0.9);
+    directionalLight2.position.set(0, 700, 600).normalize();
+    this.scene.add(directionalLight2);
+
+    const ifcLoader = new IFCLoader();
+
+    ifcLoader.load("/sample.ifc", (model) => {
+      this.scene.add(model);
+    });
+
+    this.map = map;
+
+    this.renderer = new WebGLRenderer({
+      canvas: map.getCanvas(),
+      context: gl,
+      antialias: true,
+    });
+
+    this.renderer.autoClear = false;
+  };
+  const handleRender = (gl, matrix) => {};
+
   const three: any = Three();
 
   return (
@@ -45,12 +112,9 @@ function MapEnvironment() {
         mapLib={maplibregl}
         mapStyle="https://api.maptiler.com/maps/basic-v2/style.json?key=ZDFWcNAeAKwpseiIpuuj"
       >
-        <Layer type="custom" render={(gl, matrix) => {}}>
-          <Canvas>
-            <mesh>
-              <sphereGeometry />
-            </mesh>
-          </Canvas>
+        {/* <Layer type="custom" onAdd={handleLoad}/> */}
+        <Layer type="custom" render={handleRender}>
+          <sphereBufferGeometry />
         </Layer>
       </Map>
     </>
---
+++ src/components/ThreeFiber.tsx
@@ -0,0 +1,5 @@
+import { useLoader, useThree } from "@react-three/fiber";
+import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader";
+import { IFCLoader } from "web-ifc-three";
+
+export default function ThreeFiber() {}
